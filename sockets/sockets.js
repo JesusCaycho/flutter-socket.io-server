3a2499fa0d38615c538bf3eaca1d150e80d75e38
Jesus Caycho Ramos <jans_tob_2003@hotmail.com>
2020-12-13 12:29:17 -0500

    Fin sección 3

---
+++ sockets/sockets.js
@@ -0,0 +1,13 @@
+const {io} = require('../index');
+// Mensaje de Sockets
+io.on('connection', client =>{
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => {
+        console.log('Cliente Desconectado');
+    });
+    client.on('mensaje',(payload)=> {
+        console.log('Mensaje!!!', payload);
+        io.emit('mensaje', {admin:'Nuevo mensaje'});
+    })
+});
